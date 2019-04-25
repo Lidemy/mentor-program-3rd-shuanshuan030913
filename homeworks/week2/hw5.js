bae6166cd69828dcedacee6016cb91b8e40aaf28
shuanshuan030913 <skypainter59768@gmail.com>
2019-04-25 22:19:46 +0800

    hw5 ~ hw6 OK

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,32 @@
 function join(str, concatStr) {
-  return concatStr;
+
+	var answer = "";
+
+	for(var i = 0; i < str.length - 1; i++) {
+
+		answer += str[i] + concatStr;
+	}
+
+	return answer += str[str.length - 1] ;
+
 }
 
 function repeat(str, times) {
-  return times;
+
+	var answer = "";
+
+	for(var i = 0; i < times; i++){
+		answer += str;
+	}
+
+	return answer;
 }
 
-console.log(join('a', '!'));
+
+
+console.log(join([1, 2, 3], ''));
+console.log(join(["a", "b", "c"], "!"));
+console.log(join(["a", 1, "b", 2, "c", 3], ','));
+
 console.log(repeat('a', 5));
+console.log(repeat('yoyo', 2));
