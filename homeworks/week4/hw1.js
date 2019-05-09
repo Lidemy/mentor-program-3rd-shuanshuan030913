2aabe3667b8163b2c8396b0786761ff9093f525f
shuanshuan030913 <38518475+shuanshuan030913@users.noreply.github.com>
2019-05-09 22:05:52 +0800

    Update hw1.js
    fix bug

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 request(
-  'https://lidemy-book-store.herokuapp.com/books?_limit=9',
+  'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
     const json = JSON.parse(body);
     for (let i = 0; i < json.length; i++) {
